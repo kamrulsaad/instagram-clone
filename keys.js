54b5126dba5cc93252a6345d98019fd66dbc4b63
Kamrul Huda Sattari Saad <skamrul2680@gmail.com>
2022-09-03 02:15:30 +0600

    mongodb bad auth error solved by changing user access

--- keys.js
+++ keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-    MONGOURI : "mongodb+srv://saad:iY0BzD7BH1nAAhIS@insta-cluster.g5fa1j4.mongodb.net/?retryWrites=true&w=majority"
+    MONGOURI: "mongodb+srv://newUser:saad1626@insta-cluster.g5fa1j4.mongodb.net/?retryWrites=true&w=majority"
 }
 
 // iY0BzD7BH1nAAhIS
