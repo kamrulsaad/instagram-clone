604a9553faf865d198539beaad1a6ffd9633376e
Kamrul Huda Sattari Saad <skamrul2680@gmail.com>
2022-09-02 17:24:15 +0600

    error with server connection

--- keys.js
+++ keys.js
@@ -1,4 +1,5 @@
 module.exports = {
-    MONGOURI : 'mongodb+srv://saad:5TnOKcH1BOrZiQ4f@insta-cluster.g5fa1j4.mongodb.net/?retryWrites=true&w=majority'
+    MONGOURI : "mongodb+srv://saad:iY0BzD7BH1nAAhIS@insta-cluster.g5fa1j4.mongodb.net/?retryWrites=true&w=majority"
 }
-// mongodb+srv://saad:4mAf3EBsSg6L2BiF@insta-cluster.g5fa1j4.mongodb.net/?retryWrites=true&w=majority
+
+// iY0BzD7BH1nAAhIS
