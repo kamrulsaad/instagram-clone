3173e84606495354b48284e7b21fff14a7d2082f
Kamrul Huda Sattari Saad <skamrul2680@gmail.com>
2022-09-04 00:57:55 +0600

    addded get route for my posts

--- routes/post.js
+++ routes/post.js
@@ -4,6 +4,28 @@ const mongoose = require('mongoose')
 const requireLogin = require('../middleware/requireLogin')
 const Post = mongoose.model("Post")
 
+router.get('/allPosts', (req, res) => {
+    Post.find()
+    .populate("postedBy",'_id name')
+    .then(posts => {
+        res.json(posts)
+    })
+    .catch(err => {
+        console.log(err);
+    })
+})
+
+router.get('/myPosts',requireLogin, (req, res) => {
+    Post.find({postedBy: req.user._id})
+    .populate("postedBy", "id name")
+    .then(myPosts => {
+        res.json(myPosts)
+    })
+    .catch(err => {
+        console.log(err);
+    })
+})
+
 router.post('/createPost', requireLogin, (req, res) => {
     const { title, body } = req.body
     if (!title || !body) {
