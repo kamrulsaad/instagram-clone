f4d238f5d4ef8059c75b87695712f425ed34219d
Kamrul Huda Sattari Saad <skamrul2680@gmail.com>
2022-09-04 22:39:55 +0600

    stopping this projecct right now

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import { Nav } from "./Components/Shared/Navbar";
-
+import {Routes, Route} from "react-router-dom"
+import Login from "./Components/Login/Login";
 
 function App() {
   return (
     <div>
       <Nav></Nav>
+      <Routes>
+        <Route path="/signin" element={<Login></Login>}></Route>
+      </Routes>
     </div>
   );
 }
--- client/src/Components/Shared/Navbar.js
+++ client/src/Components/Shared/Navbar.js
@@ -1,11 +1,12 @@
 import { useState } from "react";
+import {Link} from "react-router-dom"
 
 export const Nav = () => {
 
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     return (
-        <div class="bg-gray-900">
+        <div class="bg-gray-900 sticky top-0 ">
             <div class="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
                 <div class="relative flex items-center justify-between">
                     <div class="flex items-center">
@@ -15,7 +16,7 @@ export const Nav = () => {
                             title="Instagram"
                             class="inline-flex items-center mr-8">   
                             <img className="w-10" src="assets/logo.png" alt="" />
-                            <span class="ml-2 text-xl font-bold tracking-wide text-gray-100 uppercase">
+                            <span class="ml-2 grand-hotel text-3xl font-bold tracking-wide text-gray-100">
                                 Instagram
                             </span>
                         </a>
@@ -25,14 +26,14 @@ export const Nav = () => {
                     </div>
                     <ul class="items-center hidden space-x-8 lg:flex">
                         <li>
-                            <a
-                                href="/"
+                            <Link
+                                to="/signin"
                                 aria-label="Sign in"
                                 title="Sign in"
                                 class="font-medium tracking-wide text-gray-100 transition-colors duration-200 hover:text-teal-accent-400"
                             >
                                 Sign in
-                            </a>
+                            </Link>
                         </li>
                         <li>
                             <a
@@ -78,23 +79,9 @@ export const Nav = () => {
                                                 title="Company"
                                                 class="inline-flex items-center"
                                             >
-                                                <svg
-                                                    class="w-8 text-deep-purple-accent-400"
-                                                    viewBox="0 0 24 24"
-                                                    strokeLinejoin="round"
-                                                    strokeWidth="2"
-                                                    strokeLinecap="round"
-                                                    strokeMiterlimit="10"
-                                                    stroke="currentColor"
-                                                    fill="none"
-                                                >
-                                                    <rect x="3" y="1" width="7" height="12" />
-                                                    <rect x="3" y="17" width="7" height="6" />
-                                                    <rect x="14" y="1" width="7" height="6" />
-                                                    <rect x="14" y="11" width="7" height="12" />
-                                                </svg>
-                                                <span class="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
-                                                    Company
+                                                <img className="w-10" src="assets/logo.png" alt="" />
+                                                <span class="ml-2 grand-hotel text-2xl font-bold tracking-wide text-gray-800">
+                                                   Instagram
                                                 </span>
                                             </a>
                                         </div>
@@ -117,54 +104,14 @@ export const Nav = () => {
                                     <nav>
                                         <ul class="space-y-4">
                                             <li>
-                                                <a
-                                                    href="/"
-                                                    aria-label="Our product"
-                                                    title="Our product"
-                                                    class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
-                                                >
-                                                    Product
-                                                </a>
-                                            </li>
-                                            <li>
-                                                <a
-                                                    href="/"
-                                                    aria-label="Our product"
-                                                    title="Our product"
-                                                    class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
-                                                >
-                                                    Features
-                                                </a>
-                                            </li>
-                                            <li>
-                                                <a
-                                                    href="/"
-                                                    aria-label="Product pricing"
-                                                    title="Product pricing"
-                                                    class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
-                                                >
-                                                    Pricing
-                                                </a>
-                                            </li>
-                                            <li>
-                                                <a
-                                                    href="/"
-                                                    aria-label="About us"
-                                                    title="About us"
-                                                    class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
-                                                >
-                                                    About us
-                                                </a>
-                                            </li>
-                                            <li>
-                                                <a
+                                                <Link to='/signin'
                                                     href="/"
                                                     aria-label="Sign in"
                                                     title="Sign in"
                                                     class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400"
                                                 >
                                                     Sign in
-                                                </a>
+                                                </Link>
                                             </li>
                                             <li>
                                                 <a
